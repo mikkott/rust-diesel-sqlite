087922155dbd6d1be0d8396d62b1d2da2b27ed26
Mikko Turpeinen <mikko@email>
2024-02-23 20:44:18 +0200

    list all users

--- src/db/controllers.rs
+++ src/db/controllers.rs
@@ -1,14 +1,15 @@
 use crate::db::models::NewUser;
 use crate::db::models::User;
-use crate::db::schema::users::id;
+use crate::db::schema::users::email;
+
 use diesel::ExpressionMethods;
 use diesel::QueryDsl;
+use diesel::RunQueryDsl;
 use diesel::SqliteConnection;
 
 pub fn exist_user(conn: &mut SqliteConnection, e: &str) -> Option<bool> {
     use crate::db::schema::users::dsl::*;
     use diesel::dsl::exists;
-    use diesel::RunQueryDsl;
 
     let user_exists: Result<bool, diesel::result::Error> = diesel::select(exists(
         users.filter(crate::db::schema::users::dsl::email.eq(&e)),
@@ -21,12 +22,11 @@ pub fn exist_user(conn: &mut SqliteConnection, e: &str) -> Option<bool> {
     }
 }
 
-pub fn create_user(conn: &mut SqliteConnection, username: &str, email: &str) -> User {
+pub fn create_user(conn: &mut SqliteConnection, username: &str, e: &str) -> User {
     use crate::db::schema::users;
-    use diesel::RunQueryDsl;
     use diesel::SelectableHelper;
 
-    let new_user = NewUser { username, email };
+    let new_user = NewUser { username, email: e };
 
     diesel::insert_into(users::table)
         .values(&new_user)
@@ -40,7 +40,11 @@ pub fn delete_user(
     user: User,
 ) -> Result<usize, diesel::result::Error> {
     use crate::db::schema::users::dsl::users;
-    use diesel::RunQueryDsl;
 
-    diesel::delete(users.filter(id.eq(user.id))).execute(conn)
+    diesel::delete(users.filter(email.eq(user.email))).execute(conn)
+}
+
+pub fn list_users(conn: &mut SqliteConnection) -> Result<Vec<String>, diesel::result::Error> {
+    use crate::db::schema::users::dsl::users;
+    users.select(email).load::<String>(conn)
 }
--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,15 @@ fn main() {
 
     let conn: &mut SqliteConnection = &mut db::conn::conn_sqlite();
 
-    let user: User = db::models::User::new("foo@bar", "foobarri");
+    let new_user: User = db::models::User::new("delete@this", "testuser");
 
-    let user: Option<User> = match db::controllers::exist_user(conn, user.email.as_str()) {
+    let user_to_delete = &new_user;
+
+    let user: Option<User> = match db::controllers::exist_user(conn, new_user.email.as_str()) {
         Some(false) => Some(db::controllers::create_user(
             conn,
-            &user.username,
-            &user.email,
+            &new_user.username,
+            &new_user.email,
         )),
         Some(true) => None,
         None => None,
@@ -26,18 +28,44 @@ fn main() {
         None => println!("user exists, no action"),
     };
 
+    let foo: Result<Vec<String>, diesel::result::Error> = db::controllers::list_users(conn);
+
+    match foo {
+        Ok(res) => {
+            for e in res {
+                println!("{}", e);
+            }
+        }
+        Err(_) => println!("Error"),
+    };
+
     let res = db::controllers::delete_user(
         conn,
         User {
             id: 1,
             username: "foo".to_string(),
-            email: "bar".to_string(),
+            email: user_to_delete.email.clone(),
             enabled: false,
         },
     );
 
+    let foo: Result<Vec<String>, diesel::result::Error> = db::controllers::list_users(conn);
+
+    match foo {
+        Ok(res) => {
+            if res.len() > 0 {
+                for e in res {
+                    println!("{}", e);
+                }
+            } else {
+                println!("db empty");
+            }
+        }
+        Err(_) => println!("Error"),
+    };
+
     match res {
         Ok(_) => println!("delete test success"),
-        Err(_) => println!("delete test sucks"),
+        Err(_) => println!("delete test bad"),
     }
 }
